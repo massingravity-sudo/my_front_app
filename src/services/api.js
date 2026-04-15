7d377c96c3c763c60da10fb45a8442244da27041
massingravity-sudo <massingravity@gmail.com>
2026-04-15 10:50:37 +0100

    update frontend

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = `${import.meta.env.VITE_API_URL || 'http://localhost:5000'}/api`;
 
 // Intercepteur pour ajouter le token
 axios.interceptors.request.use(
